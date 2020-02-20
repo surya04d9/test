0a381f47ff10c22506ed335c51556bcf0351e350
surya04d9 <61229730+surya04d9@users.noreply.github.com>
2020-02-20 14:14:07 +0530

    java code

---
+++ Vote.java
@@ -0,0 +1,18 @@
+
+public class Vote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=0;
+if(i>=18) {
+	System.out.println("Eligible to vote");
+}
+	else{
+		System.out.println(" not Eligible to vote");
+
+	
+}
+	}
+	
+
+}
